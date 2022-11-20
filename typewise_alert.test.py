3cea17581e7ea7a581fba95718c3691ffe0dde04
chandhappy <35898839+chandhappy@users.noreply.github.com>
2022-11-20 12:46:34 +0530

    Update typewise_alert.test.py
    all test cases

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -21,6 +21,18 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(typewise_alert.classify_temperature_breach('MED_ACTIVE_COOLING', 90)== 'TOO_HIGH')
     self.assertTrue(typewise_alert.classify_temperature_breach('MED_ACTIVE_COOLING', 0)== 'NORMAL')
 
+  def test_check_and_alert(self):
+    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER',{'coolingType':'PASSIVE_COOLING'}, 100)== print('65261, TOO_HIGH'))
+    self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL',{'coolingType':'HI_ACTIVE_COOLING'}, 50)== print('To: a.b@c.com\nHi, the temperature is too high'))
+
+  def test_send_to_contoroller(self):
+    self.assertTrue(typewise_alert.send_to_controller('NORMAL') == print('65261, NORMAL'))
+    self.assertTrue(typewise_alert.send_to_controller('TOO_HIGH') == print('65261, TOO_HIGH'))
+ 
+  def test_send_to_email(self):
+    self.assertTrue(typewise_alert.send_to_email("TOO_LOW") == print("To: a.b@c.com\nHi, the temperature is too low") )
+    self.assertTrue(typewise_alert.send_to_email("NORMAL") == None)
+    self.assertTrue(typewise_alert.send_to_email("TOO_HIGH") == print("To: a.b@c.com\nHi, the temperature is too high") )
 
 
 if __name__ == '__main__':
